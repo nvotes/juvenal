4797a5f8a1191392d297d8395a01a35525023713
edulix <edulix@users.noreply.github.com>
2020-02-23 21:05:34 +0000

    deploy: 2ddc40220788ad1328df3917e85860de926f4829

---
+++ precache-manifest.a1df182b0733f0fce2c15458937a7f26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05d7b52bb42aa593a7b376d8dad8a9b",
+    "url": "/a50917d372cf2587028e.worker.js"
+  },
+  {
+    "revision": "8931d2f1ac2d21e19a3c12285c0ae491",
+    "url": "/a50917d372cf2587028e.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac7714937ddbf912301d51862eb9ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25945934e57f0d090756",
+    "url": "/static/css/main.da228218.chunk.css"
+  },
+  {
+    "revision": "6a89517bf2ebd396b794",
+    "url": "/static/js/2.374cdc08.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.374cdc08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25945934e57f0d090756",
+    "url": "/static/js/main.99b1f3a7.chunk.js"
+  },
+  {
+    "revision": "3ca46611054e7b1adb6c",
+    "url": "/static/js/runtime-main.9bab3c7e.js"
+  }
+]);
